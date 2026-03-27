50521098bad75285266806c3999b66fcffc69a99
sooch-h <haso3838@colorado.edu>
2026-03-27 15:32:47 -0600

    Add files via upload

---
+++ lab3_3802_main.m
@@ -0,0 +1,33 @@
+%Name: 3802-Lab3 Main
+%Authors: Harroop Sooch,
+%Date: 3/27/2026
+
+clear;
+clc;
+close all;
+
+%--------------------------------------------------------------------
+%toggles
+p1_task1 = 0;
+
+
+airfoil = '4415';
+
+m = str2num(airfoil(1))/100;
+p = str2num(airfoil(2))/10;
+t = str2num(airfoil(3:4))/100;
+
+
+c = 10;
+N = 10;
+r = c/2;
+x = zeros(1,10);
+dtheta = 180/(N-1);
+angle = 180;
+for i=1:N
+
+x(i) = r*cosd(angle) + r;
+
+angle = angle - dtheta;
+end
+
