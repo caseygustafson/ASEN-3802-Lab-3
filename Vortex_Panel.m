1b6f3910d8645bbad9994181f86dd9a36351b3a3
aprilcope1 <apco9505@colorado.edu>
2026-04-08 19:46:25 -0600

    Update Vortex_Panel.m

--- Vortex_Panel.m
+++ Vortex_Panel.m
@@ -1,101 +1,131 @@
-function [cl] = Vortex_Panel(XB,YB,Vinf,alpha)
-
+function [CL] = Vortex_Panel(XB,YB,ALPHA)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Input:                           %
-% XB    = Boundary Points x-location
-% YB    = Boundary Points y-location
-% Vinf  = Free-stream velocity
-% alpha = Angle of attack in degrees
-%
-% Output:
-% cl    = Sectional Lift Coefficient
+%                                  %
+% XB  = Boundary Points x-location %
+% YB  = Boundary Points y-location %
+% ALPHA = AOA in degrees           %
+%                                  %
+% Output:                          %
+%                                  %
+% CL = Sectional Lift Coefficient  %
+% improves efficiency by preallocating matrices
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-alpha = alpha*pi/180;   % convert to radians
-chord = max(XB)-min(XB);
-
-M = length(XB)-1;       % number of panels
+%%%%%%%%%%%%%%%%%%%%%%
+% Convert to Radians %
+%%%%%%%%%%%%%%%%%%%%%%
+ALPHA = ALPHA*pi/180;
+%%%%%%%%%%%%%%%%%%%%%
+% Compute the Chord %
+%%%%%%%%%%%%%%%%%%%%%
+CHORD = max(XB)-min(XB);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Determine the Number of Panels %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+M = max(size(XB,1),size(XB,2))-1;
 MP1 = M+1;
-
-% -------------------- Panel Geometry --------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Preallocate Matrices for Efficiency %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 X = zeros(1,M);
 Y = zeros(1,M);
 S = zeros(1,M);
-theta = zeros(1,M);
-sine = zeros(1,M);
-cosine = zeros(1,M);
+THETA = zeros(1,M);
+SINE = zeros(1,M);
+COSINE = zeros(1,M);
 RHS = zeros(1,M);
-
-for i = 1:M
-    ip1 = i+1;
-    X(i) = 0.5*(XB(i)+XB(ip1));
-    Y(i) = 0.5*(YB(i)+YB(ip1));
-    S(i) = sqrt((XB(ip1)-XB(i))^2 + (YB(ip1)-YB(i))^2);
-    theta(i) = atan2(YB(ip1)-YB(i), XB(ip1)-XB(i));
-    sine(i) = sin(theta(i));
-    cosine(i) = cos(theta(i));
-    RHS(i) = sin(theta(i)-alpha);
-end
-
-% -------------------- Influence Coefficients --------------------
-CN1 = zeros(M,M); CN2 = zeros(M,M);
-CT1 = zeros(M,M); CT2 = zeros(M,M);
-
-for i = 1:M
-    for j = 1:M
-        if i == j
-            CN1(i,j) = -1; CN2(i,j) = 1;
-            CT1(i,j) = 0.5*pi; CT2(i,j) = 0.5*pi;
-        else
-            A = -(X(i)-XB(j))*cosine(j) - (Y(i)-YB(j))*sine(j);
-            B = (X(i)-XB(j))^2 + (Y(i)-YB(j))^2;
-            C = sin(theta(i)-theta(j));
-            D = cos(theta(i)-theta(j));
-            E = (X(i)-XB(j))*sine(j) - (Y(i)-YB(j))*cosine(j);
-            F = log(1 + S(j)*(S(j)+2*A)/B);
-            G = atan2(E*S(j), B+A*S(j));
-            P = (X(i)-XB(j))*sin(theta(i)-2*theta(j)) + (Y(i)-YB(j))*cos(theta(i)-2*theta(j));
-            Q = (X(i)-XB(j))*cos(theta(i)-2*theta(j)) - (Y(i)-YB(j))*sin(theta(i)-2*theta(j));
-            CN2(i,j) = D + 0.5*Q*F/S(j) - (A*C+D*E)*G/S(j);
-            CN1(i,j) = 0.5*D*F + C*G - CN2(i,j);
-            CT2(i,j) = C + 0.5*P*F/S(j) + (A*D-C*E)*G/S(j);
-            CT1(i,j) = 0.5*C*F - D*G - CT2(i,j);
-        end
-    end
-end
-
-AN = zeros(MP1,MP1); AT = zeros(M,MP1);
-
-for i = 1:M
-    AN(i,1) = CN1(i,1);
-    AN(i,MP1) = CN2(i,M);
-    AT(i,1) = CT1(i,1);
-    AT(i,MP1) = CT2(i,M);
-    for j = 2:M
-        AN(i,j) = CN1(i,j)+CN2(i,j-1);
-        AT(i,j) = CT1(i,j)+CT2(i,j-1);
-    end
-end
-
-AN(MP1,1) = 1; AN(MP1,MP1) = 1;
-AN(MP1,2:M) = 0;
-RHS(MP1) = 0;
-
-% -------------------- Solve for circulation --------------------
-GAMA = AN\RHS';
-
-% -------------------- Tangential velocity & CP --------------------
+CN1 = zeros(M);
+CN2 = zeros(M);
+CT1 = zeros(M);
+CT2 = zeros(M);
+AN = zeros(M);
+AT = zeros(M);
 V = zeros(1,M);
 CP = zeros(1,M);
-for i = 1:M
-    V(i) = cos(theta(i)-alpha);
-    for j = 1:MP1
-        V(i) = V(i) + AT(i,j)*GAMA(j);
-    end
-    CP(i) = 1 - V(i)^2;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Intra-Panel Relationships:                                  %
+%                                                             %
+% Determine the Control Points, Panel Sizes, and Panel Angles %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for I = 1:M
+   IP1 = I+1;
+   X(I) = 0.5*(XB(I)+XB(IP1));
+   Y(I) = 0.5*(YB(I)+YB(IP1));
+   S(I) = sqrt( (XB(IP1)-XB(I))^2 +( YB(IP1)-YB(I))^2 );
+   THETA(I) = atan2( YB(IP1)-YB(I), XB(IP1)-XB(I) );
+   SINE(I) = sin( THETA(I) );
+   COSINE(I) = cos( THETA(I) );
+   RHS(I) = sin( THETA(I)-ALPHA );
 end
-
-% -------------------- Sectional Lift Coefficient --------------------
-cl = 2*sum(S.*V)/chord;
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Inter-Panel Relationships:             %
+%                                        %
+% Determine the Integrals between Panels %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for I = 1:M
+   for J = 1:M
+       if I == J
+           CN1(I,J) = -1.0;
+           CN2(I,J) = 1.0;
+           CT1(I,J) = 0.5*pi;
+           CT2(I,J) = 0.5*pi;
+       else
+           A = -(X(I)-XB(J))*COSINE(J) - (Y(I)-YB(J))*SINE(J);
+           B = (X(I)-XB(J))^2 + (Y(I)-YB(J))^2;
+           C = sin( THETA(I)-THETA(J) );
+           D = cos( THETA(I)-THETA(J) );
+           E = (X(I)-XB(J))*SINE(J) - (Y(I)-YB(J))*COSINE(J);
+           F = log( 1.0 + S(J)*(S(J)+2*A)/B );
+           G = atan2( E*S(J), B+A*S(J) );
+           P = (X(I)-XB(J)) * sin( THETA(I) - 2*THETA(J) ) ...
+             + (Y(I)-YB(J)) * cos( THETA(I) - 2*THETA(J) );
+           Q = (X(I)-XB(J)) * cos( THETA(I) - 2*THETA(J) ) ...
+             - (Y(I)-YB(J)) * sin( THETA(I) - 2*THETA(J) );
+           CN2(I,J) = D + 0.5*Q*F/S(J) - (A*C+D*E)*G/S(J);
+           CN1(I,J) = 0.5*D*F + C*G - CN2(I,J);
+           CT2(I,J) = C + 0.5*P*F/S(J) + (A*D-C*E)*G/S(J);
+           CT1(I,J) = 0.5*C*F - D*G - CT2(I,J);
+       end
+   end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Inter-Panel Relationships:           %
+%                                      %
+% Determine the Influence Coefficients %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for I = 1:M
+   AN(I,1) = CN1(I,1);
+   AN(I,MP1) = CN2(I,M);
+   AT(I,1) = CT1(I,1);
+   AT(I,MP1) = CT2(I,M);
+   for J = 2:M
+       AN(I,J) = CN1(I,J) + CN2(I,J-1);
+       AT(I,J) = CT1(I,J) + CT2(I,J-1);
+   end
+end
+AN(MP1,1) = 1.0;
+AN(MP1,MP1) = 1.0;
+for J = 2:M
+   AN(MP1,J) = 0.0;
+end
+RHS(MP1) = 0.0;
+%%%%%%%%%%%%%%%%%%%%%%%%
+% Solve for the gammas %
+%%%%%%%%%%%%%%%%%%%%%%%%
+GAMA = AN\RHS';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Solve for Tangential Veloity and Coefficient of Pressure %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for I = 1:M
+   V(I) = cos( THETA(I)-ALPHA );
+   for J = 1:MP1
+       V(I) = V(I) + AT(I,J)*GAMA(J);
+   end
+   CP(I) = 1.0 - V(I)^2;
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Solve for Sectional Coefficient of Lift %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+CIRCULATION = sum(S.*V);
+CL = 2*CIRCULATION/CHORD;
 end
