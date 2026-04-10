b9ab433f0af121c22a4ac3564aac572a385f6674
caseygustafson <cagu6020@colorado.edu>
2026-04-10 14:01:55 -0600

    PLLT added

---
+++ PLLT.m
@@ -0,0 +1,11 @@
+function [e,c_L,c_Di] = PLLT(b,a0_t,a0_r,c_t,c_r,aero_t,aero_r,geo_t,geo_r,N)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+theta = zeros(N);
+
+for i=1:N
+    theta = i * pi / (2*N_p);
+end
+
+end
+
