6061328764af1267183db17c3469860da9b76a62
caseygustafson <cagu6020@colorado.edu>
2026-04-12 19:27:23 -0600

    Update

--- PLLT.m
+++ PLLT.m
@@ -1,4 +1,4 @@
-function [e,c_L,c_Di,ass,balls] = PLLT(b,a0_t,a0_r,c_t,c_r,aero_t,aero_r,geo_t,geo_r,N)
+function [e,c_L,c_Di] = PLLT(b,a0_t,a0_r,c_t,c_r,aero_t,aero_r,geo_t,geo_r,N)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
